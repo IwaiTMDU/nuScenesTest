b6130d5a696a17a95706a0e9bec4d6b8552e1439
IwaiTMDU <takuya.iwai.p@tier4.jp>
2021-01-07 20:21:31 +0900

    distance

--- nuscenes_viewer.py
+++ nuscenes_viewer.py
@@ -1,5 +1,6 @@
 from nuscenes.nuscenes import NuScenes
-from nuscenes.utils.data_classes import RadarPointCloud
+from nuscenes.utils.data_classes import RadarPointCloud, PointCloud
+from nuscenes.utils.geometry_utils import view_points
 import numpy as np
 import os
 import matplotlib.pyplot as plt
@@ -9,9 +10,12 @@ import open3d as o3d
 import cv2
 import io
 from PIL import Image
+from pyquaternion import Quaternion
+import copy
 
 
 dataroot = "v1.0-mini"
+'''
 class_names = [
     'bg' ,
     'animal',
@@ -38,14 +42,30 @@ class_names = [
     'vehicle.trailer',
     'vehicle.truck'
 ]
+'''
+
+class_names = [
+    'bg' ,
+    'vehicle.bicycle',
+    'vehicle.bus.bendy',
+    'vehicle.bus.rigid',
+    'vehicle.car',
+    'vehicle.construction',
+    'vehicle.emergency.ambulance',
+    'vehicle.emergency.police',
+    'vehicle.motorcycle',
+    'vehicle.trailer',
+    'vehicle.truck'
+]
 
 class_to_color = {}
 
-def put_bbox_into_image(annotation):
+def put_bbox_into_image(annotation, radar_in_image = None, rcs_colors = None):
     font                   = cv2.FONT_HERSHEY_SIMPLEX
     fontScale              = 1.0
 
     image = cv2.imread(annotation["image_file"])
+    
     for data in annotation["annotations"]:
         if not (data["label"] in class_to_color):
             continue
@@ -53,12 +73,24 @@ def put_bbox_into_image(annotation):
         color =class_to_color[data["label"]] * 255
         cv2.rectangle(image,(x1, y1), (x2, y2), color,2)
         put_class_label = data["label"].split('.')[-1]
-        (retval,baseLine) = cv2.getTextSize(put_class_label, font, fontScale,1)
+        text = put_class_label
+        if data["distance"] is not None:
+            text += "[{}m]".format(round(data["distance"], 1))
+        (retval,baseLine) = cv2.getTextSize(text, font, fontScale,1)
         textOrg = int(x1), int(y1)
 
         cv2.rectangle(image, (textOrg[0] - 1,textOrg[1]+baseLine - 1), (textOrg[0]+retval[0] + 1, textOrg[1]-retval[1] - 1), color, -1)
-        cv2.putText(image, put_class_label, textOrg, cv2.FONT_HERSHEY_SIMPLEX, fontScale, (1,1,1), 1)
-    
+        cv2.putText(image, text, textOrg, cv2.FONT_HERSHEY_SIMPLEX, fontScale, (1,1,1), 1)
+        
+    if (radar_in_image is not None) and (rcs_colors is not None):
+        for point_index in range(radar_in_image.shape[1]):
+            point = radar_in_image[:,point_index]
+            u, v = int(point[0]), int(point[1])
+            point_color = rcs_colors[token_index][point_index]
+            point_color = (int(point_color[0]), int(point_color[1]), int(point_color[2]))
+            if ((0 <= u <= image.shape[1]) and (0 <= v <= image.shape[0])): # point is in the image
+                cv2.circle(image, (u, v), 6, color = point_color, thickness = -1, lineType = cv2.LINE_8)
+
     return image
 
 def get_annotation_bbox(nusc, tokens):
@@ -187,6 +219,75 @@ def get_radar_points(nusc, tokens):
 
     return radar_points, radar_meta_data
 
+def radar_point_to_image(nusc, tokens, radar_points):
+    radar_in_image = []
+    for token_index in tqdm(range(len(tokens))):
+        token = tokens[token_index]
+        sample = nusc.get('sample', token)
+        camera_rec = nusc.get('sample_data', sample['data']["CAM_FRONT"])
+        radar_rec = nusc.get('sample_data', sample['data']["RADAR_FRONT"])
+        pc = PointCloud(copy.deepcopy(radar_points[token_index]))
+
+        # Points live in the point sensor frame. So they need to be transformed via global to the image plane.
+        # First step: transform the point-cloud to the ego vehicle frame for the timestamp of the sweep.
+        cs_record = nusc.get('calibrated_sensor', radar_rec['calibrated_sensor_token'])
+        pc.rotate(Quaternion(cs_record['rotation']).rotation_matrix)
+        pc.translate(np.array(cs_record['translation']))
+
+        # Second step: transform to the global frame.
+        poserecord = nusc.get('ego_pose', radar_rec['ego_pose_token'])
+        pc.rotate(Quaternion(poserecord['rotation']).rotation_matrix)
+        pc.translate(np.array(poserecord['translation']))
+
+        # Third step: transform into the ego vehicle frame for the timestamp of the image.
+        poserecord = nusc.get('ego_pose', camera_rec['ego_pose_token'])
+        pc.translate(-np.array(poserecord['translation']))
+        pc.rotate(Quaternion(poserecord['rotation']).rotation_matrix.T)
+
+        # Fourth step: transform into the camera.
+        cs_record = nusc.get('calibrated_sensor', camera_rec['calibrated_sensor_token'])
+        pc.translate(-np.array(cs_record['translation']))
+        pc.rotate(Quaternion(cs_record['rotation']).rotation_matrix.T)
+
+        # [u, v, 1]
+        view = np.array(cs_record['camera_intrinsic'])
+        points = view_points(pc.points, view, normalize=True).astype(np.int32)
+        radar_in_image.append(points)
+
+    return radar_in_image
+
+def get_rcs_color(tokens, radar_meta_data):
+    colors = []
+    for token_index in tqdm(range(len(tokens))):
+        rcs_positive = np.power(10, radar_meta_data[token_index][2,:]/20.0)
+        max_rcs = np.max(rcs_positive)
+
+        rcs_hue = 120*(max_rcs - rcs_positive).astype(np.uint8)
+        rcs_hsv = 255*np.ones((rcs_hue.shape[0], 3)).astype(np.uint8)
+        rcs_hsv[:,0] = rcs_hue
+        
+        rcs_colors = np.array([cv2.cvtColor(np.array([[rcs_hsv[point_idx]]], dtype=np.uint8), cv2.COLOR_HSV2BGR) for point_idx in range(rcs_hsv.shape[0])])
+        rcs_colors = np.reshape(rcs_colors, (rcs_colors.shape[0],3))
+        #rcs_colors = np.concatenate([rcs_colors, 255*np.ones((rcs_colors.shape[0],1))], 1)
+        colors.append(rcs_colors)
+
+    return colors
+
+
+def check_radar_in_2dbbox(tokens, annotations, radar_in_image):
+    for token_index in tqdm(range(len(tokens))):
+        for annotation in annotations[token_index]["annotations"]:
+            box = annotation["box"]
+            annotation["radar_indexes"] = []
+            for point_index in range(radar_in_image[token_index].shape[1]):
+                point = radar_in_image[token_index][:,point_index]
+                u, v = int(point[0]), int(point[1])
+                if((box[0] <= u <= box[2]) and (box[1] <= v <= box[3])):
+                    annotation["radar_indexes"].append(point_index)
+            
+    return annotations
+
+
 if __name__ == "__main__":
     nusc = NuScenes(version = "v1.0-mini", dataroot = dataroot, verbose=False)
     scene_num = len(nusc.scene)
@@ -194,6 +295,7 @@ if __name__ == "__main__":
     prog = 0
     save_dir = "result"
     os.makedirs(save_dir, exist_ok=True)
+    distance_errors = []
 
     # Assign color
     class_to_color['bg'] = np.zeros(3)
@@ -218,6 +320,9 @@ if __name__ == "__main__":
 
     annotations = get_annotation_bbox(nusc, sample_tokens)
     radar_points, radar_meta_data = get_radar_points(nusc, sample_tokens)
+    radar_in_image = radar_point_to_image(nusc, sample_tokens, radar_points)
+    rcs_colors = get_rcs_color(sample_tokens, radar_meta_data);
+    annotations = check_radar_in_2dbbox(sample_tokens, annotations, radar_in_image)
     
     out_imgs = []
 
@@ -233,20 +338,12 @@ if __name__ == "__main__":
     out_vid = cv2.VideoWriter("result.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 2, out_vid_shape)
     for token_index in tqdm(range(len(sample_tokens))):
         radar_point = radar_points[token_index]
-        rcs_positive = np.power(10, radar_meta_data[token_index][2,:]/20.0)
-        max_rcs = np.max(rcs_positive)
 
         plt.clf()
         plt.xlabel("y [m]")
         plt.ylabel("x(forward) [m]")
-        rcs_hue = 120*(max_rcs - rcs_positive).astype(np.uint8)
-        rcs_hsv = 255*np.ones((rcs_hue.shape[0], 3)).astype(np.uint8)
-        rcs_hsv[:,0] = rcs_hue
         
-        rcs_colors = np.array([cv2.cvtColor(np.array([[rcs_hsv[point_idx]]], dtype=np.uint8), cv2.COLOR_HSV2RGB) for point_idx in range(rcs_hsv.shape[0])])
-        rcs_colors = np.reshape(rcs_colors, (rcs_colors.shape[0],3))
-        rcs_colors = np.concatenate([rcs_colors, 255*np.ones((rcs_colors.shape[0],1))], 1)
-        plt.scatter(-radar_point[1,:], radar_point[0,:], c = rcs_colors.astype(np.float)/255.0, s = scatter_size)
+        plt.scatter(-radar_point[1,:], radar_point[0,:], c = "black", s = scatter_size)
 
         vx_vy_comp = radar_meta_data[token_index][5:7,:]*4
         #vx_vy_comp = vx_vy_comp/(np.linalg.norm(vx_vy_comp, axis=0)+1e-5)*5
@@ -256,16 +353,30 @@ if __name__ == "__main__":
             if not (data["label"] in class_to_color):
                 continue
             color = class_to_color[data["label"]][::-1]
+            radar_indexes = data["radar_indexes"]
             corner = data["bev_box"]
+            data["distance"] = None
             for i_corner in range(4):
                 plt.plot([-corner[1][i_corner], -corner[1][(i_corner+1)%4]], [corner[0][i_corner], corner[0][(i_corner+1)%4]], 'k-', c = np.concatenate([color, [1]]), linewidth = 0.7)  
-        
+            if len(radar_indexes) > 0:
+                np_points = np.array(radar_point[:,radar_indexes])
+                dists = np.linalg.norm(np_points, axis=0)
+                if False:
+                    dist = dists.min()
+                else:
+                    dist = dists.mean()
+                data["distance"] = dist
+                gt_dist = np.linalg.norm(data["bev_box"])
+                distance_errors.append(gt_dist - data["distance"])
+
+                plt.scatter(-radar_point[1,radar_indexes], radar_point[0,radar_indexes], c = [color], s = scatter_size)
+
         bev_im_buf = io.BytesIO()
         #plt.legend(handles=legends, bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0, fontsize=18)
         plt.savefig(bev_im_buf, format='jpg', bbox_inches='tight')
         bev_im = cv2.imdecode(np.frombuffer(bev_im_buf.getvalue(), dtype=np.uint8), 1)
 
-        cam_img = put_bbox_into_image(annotations[token_index])
+        cam_img = put_bbox_into_image(annotations[token_index], radar_in_image=radar_in_image[token_index], rcs_colors = rcs_colors)
         asp = cam_img.shape[0]/bev_im.shape[0]
         bev_im = cv2.resize(bev_im, dsize=(round(asp*bev_im.shape[1]), cam_img.shape[0]))
         out_img = cv2.hconcat([cam_img, bev_im])
@@ -275,3 +386,4 @@ if __name__ == "__main__":
         out_vid.write(out_img)
 
     out_vid.release()
+    print("MAE: {}, std: {}".format(np.abs(distance_errors).mean(), np.std(np.abs(distance_errors))))
